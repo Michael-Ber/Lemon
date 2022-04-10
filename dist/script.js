fa559d528dd9d813a5f8eed230a7d709472d30ac
Бершадский Михаил Сергеевич <Michail.Bershadsky@ilimtimber.corp>
2022-04-10 14:54:36 +0800

    stopped on section 8

--- dist/script.js
+++ dist/script.js
@@ -93,7 +93,23 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-console.log('Test');
+window.addEventListener('DOMContentLoaded', () => {
+  //Circle diagram
+  const circlesWrapper = document.querySelectorAll('.recipes__item-diagrams__item--graph-circle');
+  const circles = document.querySelectorAll('.circle'); //Set attribute
+
+  circlesWrapper.forEach(wrapper => {
+    for (let i = 0; i < wrapper.children.length; i++) {
+      const ratio = Number(wrapper.children[1].textContent.replace('%', '')) / 100 + 'em';
+      console.log(wrapper.children[1].textContent, ratio);
+      wrapper.children[0].setAttribute('data-ratio', ratio);
+    }
+  });
+  circles.forEach(item => {
+    const size = item.getAttribute('data-ratio');
+    item.style.clip = `rect(0, ${size}, 1em, 0)`;
+  });
+});
 
 /***/ })
 
